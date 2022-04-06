cb6d1b94aa7ce348f26a0a5be26e987ac72b1172
Chris <c_guerra@baylor.edu>
2022-04-06 17:18:17 -0500

    word cloud

--- nyc.py
+++ nyc.py
@@ -1,14 +1,7 @@
-from hashlib import new
-from types import new_class
-
-from numpy import full
 from nyc_trends import nyc_trends
-from pathlib import Path
 from wordcloud import WordCloud
-import imageio
 import matplotlib.pyplot as plt
 import pandas as pd
-from textblob import TextBlob
 from operator import itemgetter
 
 #1) Using this information, create a bar chart of the top 10 topics based on their corresponding
@@ -35,3 +28,24 @@ plt.show()
 
 #2) Create a Word Cloud of all topics with over 20,000 tweet volume. The size of the word (topic)
 #should be based on their tweet volume.
+
+full1_dict = {}
+for i in nyc_trends:
+  trends1 = i["trends"]
+  for i in trends1:
+    name1 = i["name"]
+    volume1 = i["tweet_volume"]
+    if volume1 == None:
+      volume1 = 0
+    full1_dict[name1] = volume1
+    pairs = full1_dict.items()
+  filtered_dict = {key: value for key, value in pairs if value > 20000}  
+print(filtered_dict)
+
+wordcloud = WordCloud(colormap='prism',background_color='white')
+
+wordcloud = wordcloud.generate_from_frequencies(filtered_dict)
+
+plt.imshow(wordcloud)
+plt.show()
+print("Done")
