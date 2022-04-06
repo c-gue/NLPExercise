6651051bee404790bec84ad96dbf177211e238d2
Chris <c_guerra@baylor.edu>
2022-04-06 15:26:02 -0500

    finally done with number 1

--- nyc.py
+++ nyc.py
@@ -1,3 +1,7 @@
+from hashlib import new
+from types import new_class
+
+from numpy import full
 from nyc_trends import nyc_trends
 from pathlib import Path
 from wordcloud import WordCloud
@@ -9,12 +13,25 @@ from operator import itemgetter
 
 #1) Using this information, create a bar chart of the top 10 topics based on their corresponding
 # tweet volume.
-for trend in nyc_trends:
-  trends = trend["trends"]
-  df = pd.DataFrame(trends)
-  #sorted_nyc = sorted(df)
-  sorted_nyc = sorted(df, reverse=True)
-  print(sorted_nyc)
+full_list = []
+for i in nyc_trends:
+  trends = i["trends"]
+  for i in trends:
+    name = i["name"]
+    volume = i["tweet_volume"]
+    if volume == None:
+      volume = 0
+    new_list = [name,volume]
+    full_list.append(new_list)
+  sorted_list = sorted(full_list, key=itemgetter(1),reverse=True)
+  top_10 = sorted_list[:10]
+  df = pd.DataFrame(top_10,columns = ['Name','Volume'])
+  print(df)
+
+df.plot.bar(x='Name', y='Volume', legend=False)
+
+plt.gcf().tight_layout()
+plt.show()
 
 #2) Create a Word Cloud of all topics with over 20,000 tweet volume. The size of the word (topic)
 #should be based on their tweet volume.
